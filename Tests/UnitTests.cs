fcd1a3d5819d438fc15d59183127b2fe486d0d34
Matthew Schlatter <51459912+Schlattermr@users.noreply.github.com>
2024-11-06 12:35:21 -0600

    fixed test that was intentionally wrong

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -36,7 +36,7 @@ namespace GithubActionsLab
         {
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
-            Assert.AreEqual(5, Program.Subtract("5", "5"));
+            Assert.AreEqual(0, Program.Subtract("5", "5"));
         }
     }
 }
